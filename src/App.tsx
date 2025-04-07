7079c96259818bd22342daec2a9466f746afe006
Shrinjoy Saha <shrinjoysaha@Shrinjoys-MacBook-Air.local>
2025-04-07 16:45:27 +0530

    add solana wallet

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,23 @@ import LandingPage from './Landing';
 import './App.css';
 import Game from "./Game";
 import Account from "./Account";
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 
 function App() {
 
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/game" element={<Game />} />
-      <Route path="/account" element={<Account />} />
-    </Routes>
+    <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+      <WalletProvider wallets={[]} autoConnect>
+        <WalletModalProvider>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/game" element={<Game />} />
+            <Route path="/account" element={<Account />} />
+          </Routes>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   )
 }
 
