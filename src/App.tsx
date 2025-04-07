b79fa67270bb38cc95a6f54345287da8de9804cc
Shrinjoy Saha <shrinjoysaha@Shrinjoys-MacBook-Air.local>
2025-04-07 20:08:59 +0530

    add toast message, fab-icon

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,27 @@ import LandingPage from './Landing';
 import './App.css';
 import Game from "./Game";
 import Account from "./Account";
+import { ToastContainer } from 'react-toastify';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 
 function App() {
 
   return (
-    <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
-      <WalletProvider wallets={[]} autoConnect>
-        <WalletModalProvider>
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/game" element={<Game />} />
-            <Route path="/account" element={<Account />} />
-          </Routes>
-        </WalletModalProvider>
-      </WalletProvider>
-    </ConnectionProvider>
+    <>
+      <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+        <WalletProvider wallets={[]} autoConnect>
+          <WalletModalProvider>
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/game" element={<Game />} />
+              <Route path="/account" element={<Account />} />
+            </Routes>
+          </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+      <ToastContainer />
+    </>
   )
 }
 
